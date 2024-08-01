c979a59b52335791888711ec4b0cf67e6d1458f0
Dantalian <1962113203@qq.com>
2024-08-01 21:40:02 +0800

    后端项目

---
+++ src/main/java/com/dantalian/danoj/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.dantalian.danoj.controller;
+
+import com.dantalian.danoj.common.BaseResponse;
+import com.dantalian.danoj.common.ErrorCode;
+import com.dantalian.danoj.common.ResultUtils;
+import com.dantalian.danoj.exception.BusinessException;
+import com.dantalian.danoj.model.dto.postthumb.PostThumbAddRequest;
+import com.dantalian.danoj.model.entity.User;
+import com.dantalian.danoj.service.PostThumbService;
+import com.dantalian.danoj.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/lidantalian">程序员鱼皮</a>
+ * @from <a href="https://dantalian.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
