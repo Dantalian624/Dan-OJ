77e58e5d08abf232cd8c72749e0bbd7708c8b025
Dantalian624 <1962113203@qq.com>
2024-09-28 12:57:17 +0800

    CodeSandbox

--- src/main/java/com/dantalian/ojcodesandbox/SimpleCompute.java
+++
@@ -1,9 +0,0 @@
-package com.dantalian.ojcodesandbox;
-
-public class SimpleCompute {
-    public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        System.out.println("结果:" + (a + b));
-    }
-}
